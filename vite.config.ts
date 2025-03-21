0c3132c6816cd7eb58af3b0666d9dfcaf731e3d3
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2025-03-21 19:49:39 -0300

    test with pwa module
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   // Base path for GitHub Pages deployment
   base: '/scrabble-clock/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: process.env.NODE_ENV === 'development' },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
